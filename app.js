d97b1128e8e8825d88258da84033b717b7ee48ed
Asit Rohan Dass <asitdass33@gmail.com>
2023-06-12 01:46:57 +0530

    changing to light mode by default

--- app.js
+++ app.js
@@ -8,6 +8,6 @@
         })
     });
     document.querySelector(".theme-btn").addEventListener("click", () => {
-        document.body.classList.toggle("light-mode");
+        document.body.classList.toggle("dark-mode");
     })
 })();
